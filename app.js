ebbddd459f6e14538bb6b022f1c21718ef3dd9ad
Adnan Khan <adnan7.zain@gmail.com>
2022-01-05 18:24:41 +0500

    refactor connect

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('./db/connect');
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
 
 app.use(express.json());
 
@@ -14,4 +15,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on ${port}...`));
+const start = async () => {
+  try {
+    await connectDB();
+    app.listen(port, console.log(`Server is listening on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://adnan:adnan44-27@nodeexpressproject.rrx9e.mongodb.net/task-manager?retryWrites=true&w=majority';
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => console.log('connected to the db'))
-  .catch((err) => console.log(err));
+  });
+};
+
+module.exports = connectDB;
