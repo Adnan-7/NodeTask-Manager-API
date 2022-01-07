7a53e4d5003ce02ff6189285f861797a71d329cb
Adnan Khan <adnan7.zain@gmail.com>
2022-01-07 17:16:57 +0500

    add front end

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
 
+// middleware
+app.use(express.static('./public'));
 app.use(express.json());
 
 // routes
